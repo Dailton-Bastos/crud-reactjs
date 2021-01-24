ff4df7d47f6f67a595b31a1772029e702ddb588b
Dailton Bastos <dailtonbastos@gmail.com>
2021-01-24 17:19:20 -0300

    logo component

--- app/src/components/template/Logo/index.js
+++ app/src/components/template/Logo/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import LogoImg from '../../../assets/images/logo.svg';
 import './style.css';
 
 const Logo = () => {
-  return <aside className="logo">Logo</aside>;
+  return (
+    <aside className="logo">
+      <a href="/" className="logo">
+        <img src={LogoImg} alt="Logo" />
+      </a>
+    </aside>
+  );
 };
 
 export default Logo;
